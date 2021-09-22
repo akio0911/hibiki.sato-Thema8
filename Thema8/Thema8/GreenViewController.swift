be6e8b6571fe8b17ea7ea1980cda19a1a90e7351
akio0911 <akio0911@gmail.com>
2021-09-22 18:15:27 +0900

    feedback

--- Thema8/Thema8/GreenViewController.swift
+++ Thema8/Thema8/GreenViewController.swift
@@ -11,15 +11,19 @@ class GreenViewController: UIViewController {
 
     @IBOutlet private weak var label: UILabel!
     @IBOutlet private weak var slider: UISlider!
+
+    private var delegate: AppDelegate {
+        // swiftlint:disable:next force_cast
+        UIApplication.shared.delegate as! AppDelegate
+    }
+
     @IBAction private func changeSlider(_ sender: Any) {
-        let delegate = UIApplication.shared.delegate as? AppDelegate
-        delegate!.numLabel = slider.value
-        label.text = String(delegate!.numLabel)
+        delegate.sharedNumber = slider.value
+        label.text = String(delegate.sharedNumber)
     }
 
     override func viewWillAppear(_ animated: Bool) {
-        let delegate = UIApplication.shared.delegate as? AppDelegate
-        slider.value = delegate!.numLabel
-        label.text = String(delegate!.numLabel)
+        slider.value = delegate.sharedNumber
+        label.text = String(delegate.sharedNumber)
     }
 }
